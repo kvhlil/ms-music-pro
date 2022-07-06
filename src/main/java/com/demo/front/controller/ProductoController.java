bffbe32f153e21bedf587ba6ea8fe708abe847be
kahlil <kahlilxdk@gmail.com>
2022-07-05 23:23:26 -0400

    Add cross origin

--- src/main/java/com/demo/front/controller/ProductoController.java
+++ src/main/java/com/demo/front/controller/ProductoController.java
@@ -3,6 +3,7 @@ package com.demo.front.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import com.demo.front.service.IProductoService;
 
 @RestController
 @RequestMapping("/producto")
+@CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
 public class ProductoController {
 	
 	@Autowired
